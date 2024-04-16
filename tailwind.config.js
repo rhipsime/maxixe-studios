12efe14804348a5a0f8a23ff38e0fc50aa750ca1
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-04-16 22:21:36 +0100

    added sizes and colours to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Define custom screen sizes
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      }, 
+      colors: {
+        'blue': '#348AC7',
+        'yellow': '#FFC95F',
+        'cream': '##FAF9F8',
+        'white': '##FFFFFF',
+        'black': '#000000',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
+
 
