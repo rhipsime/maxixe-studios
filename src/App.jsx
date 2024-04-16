1c8c4106da99696e71f883631bbef6d731203194
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-04-16 22:43:55 +0100

    only have 1 app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import Header from './components/Header';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Horoscope from './pages/Horoscope';
+import logo from './Assets/logo.png';
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to Maxixe Studios - Your source for beautiful jewellery and Horoscope insights!
+          </p>
+          <a
+            className="App-link"
+            href="https://maxixestudios.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Explore Now
+          </a>
+        </header>
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/products" component={Products} />
-          <Route path="/horoscope" component={Horoscope} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact component={<Home />} />
+          <Route path="/products" component={<Products/>} />
+          <Route path="/horoscope" component={<Horoscope />} />
+        </Routes>
       </div>
     </Router>
   );
