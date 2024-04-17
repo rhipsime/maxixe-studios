00662d21d68018019d0d1e33f25fde4f7b1e9b6e
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-04-17 23:24:50 +0100

    colour update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import logo from '/Users/arie/maxixe-studios/src/Assets/logo.png';
 
 function Header() {
   return (
-    <header style={{ background: '#FFC95F', padding: '10px 0', borderBottom: '1px solid #dee2e6' }}>
+    <header style={{ background: '#FFC837', padding: '10px 0', borderBottom: '1px solid #dee2e6' }}>
       <div style={{ maxWidth: '1200px', margin: '0 auto', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <Link to="/" style={{ textDecoration: 'none', color: '#343a40', fontSize: '1.5rem' }}>
           <img src= {logo} alt="Logo" style={{ width: '30px', marginRight: '10px' }} />
