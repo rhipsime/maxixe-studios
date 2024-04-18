194a5e6b5dd207c6102186c21f11e02688d485ed
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-04-18 21:31:43 +0100

    home page background image css styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,46 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import CarouselComponent from '/Users/arie/maxixe-studios/src/components/CarouselComponent.jsx'; // Import the carousel component
+import backgroundImageAbove from '/Users/arie/maxixe-studios/src/Assets/background-image-above-caraousel.png'; // Import the background image above the carousel
+import backgroundImageBelow from '/Users/arie/maxixe-studios/src/Assets/background-image-under-caraousel.png'; // Import the background image below the carousel
+
 
 function Home() {
     return (
-        <div style={{ backgroundColor: '#FAF9F8', paddingTop: '200px', paddingBottom: '200px' }}> {/* Background color under the header */}
-            <Container>
-                <Row>
-                    <Col md={1}></Col> {/* Empty column on the left */}
-                    <Col md={10}>
-                        <CarouselComponent /> {/* Use the carousel component */}
-                    </Col>
-                    <Col md={1}></Col> {/* Empty column on the right */}
-                </Row>
-            </Container>
+        <div>
+            {/* Background image above the carousel */}
+            <div style={{ 
+                backgroundImage: `url(${backgroundImageAbove})`,
+                backgroundSize: 'contain',
+                paddingTop: '200px', 
+                paddingBottom: '100px',
+                width: '100%' // Ensure the div stretches out across the webpage
+            }}>
+                <Container>
+                    <Row>
+                        <Col md={1}></Col> {/* Empty column on the left */}
+                        <Col md={10}>
+                            <CarouselComponent /> {/* Use the carousel component */}
+                        </Col>
+                        <Col md={1}></Col> {/* Empty column on the right */}
+                    </Row>
+                </Container>
+            </div>
+            {/* Background image below the carousel */}
+            <div style={{ 
+                backgroundImage: `url(${backgroundImageBelow})`,
+                backgroundSize: 'cover', // Change to 'cover'
+                backgroundRepeat: 'no-repeat',
+                paddingTop: '200px', 
+                paddingBottom: '200px',
+                width: '100%' // Ensure the div stretches out across the webpage
+            }}>
+                {/* Content below the carousel */}
+            </div>
         </div>
     );
 }
 
 export default Home;
+        
+ 
